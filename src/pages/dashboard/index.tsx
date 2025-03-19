e301bc5a6d80e81dea68fb3e8ccfe6319b643520
Fernando Barraza Quintero <60108690+fernandobq@users.noreply.github.com>
2025-03-19 11:41:23 -0700

    Revert "Fb/ft tailwind pages layout (#1)"
    This reverts commit de3ec225143e8999c798695f9d6c31defa1ff16d.

--- src/pages/dashboard/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from 'react'
-
-const Dashboard: FunctionComponent = () => {
-    return <div className="text-3xl bg-amber-700 text-primary">this is the dashboard</div>
-}
-
-export default Dashboard
