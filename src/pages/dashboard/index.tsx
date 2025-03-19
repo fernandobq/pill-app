e8535577a0e747ff9c5d203832506347d23f11db
Fernando Barraza Quintero <febaqui1008@gmail.com>
2025-03-19 00:13:38 -0700

    Changed to pages route

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react'
+
+const Dashboard: FunctionComponent = () => {
+    return <div className="text-3xl bg-amber-700 text-primary">this is the dashboard</div>
+}
+
+export default Dashboard
